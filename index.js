83543e85ee747befda5cf07def25c8e3d6c92a9c
eliamiguel <eliasperreiramiguel@gmail.com>
2025-09-10 22:22:34 -0300

    Fix CORS configuration for new Vercel domain

--- index.js
+++ index.js
@@ -16,18 +16,50 @@ const app = express()
 const port = process.env.PORT || 8004;
 
 const corsOptions = {
-  origin: ['https://front-end-financa-pessoal-ycvr.vercel.app', 'http://localhost:3000'],
+  origin: [
+    'https://front-end-financa-pessoal-ycvr.vercel.app', 
+    'https://front-end-financa-pessoal-gwve.vercel.app',
+    'http://localhost:3000'
+  ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: [
     "Content-Type",
     "Authorization",
-    "Access-Control-Allow-Credentials"
+    "Access-Control-Allow-Credentials",
+    "Access-Control-Allow-Origin",
+    "Access-Control-Allow-Methods",
+    "Access-Control-Allow-Headers"
   ]
 };
 
 app.use(express.json());
 app.use(cors(corsOptions));
+
+// Middleware adicional para CORS
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    'https://front-end-financa-pessoal-ycvr.vercel.app', 
+    'https://front-end-financa-pessoal-gwve.vercel.app',
+    'http://localhost:3000'
+  ];
+  
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParse());
 
